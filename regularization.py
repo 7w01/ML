ea82212d40f406165b42e5ec833f7fb29e50cb8e
7w01 <jiangwenqi32@gmail.com>
2024-01-15 11:02:59 +0800

    add comments

--- regularization.py
+++ regularization.py
@@ -4,16 +4,18 @@ from sklearn import linear_model
 
 # input data
 data = np.genfromtxt('data1.csv', delimiter=',')
-print(data.shape)
 x_data = data[:, 2:]
 y_data = data[:, 1]
 
-lambda_test = np.linspace(0.001, 1)
+# ridge coefficents
+lambda_test = np.linspace(0.001, 1)# cross-validation
 
+# modeling
 model = linear_model.RidgeCV(alphas=lambda_test, store_cv_values=True)
 model.fit(x_data, y_data)
+print(model.alpha_)# alpha_: best ridge coefficent
 
-print(model.alpha_)
-plt.plot(lambda_test, model.cv_values_.mean(axis=0))
+# graph
+plt.plot(lambda_test, model.cv_values_.mean(axis=0)) # cv_values: matrix of loss values
 plt.plot(model.alpha_, min(model.cv_values_.mean(axis=0)), 'ro')
 plt.show()
